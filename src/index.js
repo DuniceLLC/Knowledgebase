11a82742ee6aa9471e92566f4a7070f3b2316e13
Igor Kostyakov <i.kostyakov@dunice.net>
2021-04-27 13:52:16 +0300

    rework to html to support github pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const md = new MarkdownIt();
 
+const html = fs.readFileSync(path.resolve(__dirname, 'index.html')).toString();
 const fileNames = fs.readdirSync(path.resolve(__dirname, 'docs'));
 
 for (const fileName of fileNames) {
   const data = fs.readFileSync(path.resolve(__dirname, 'docs', fileName)).toString();
   const result = md.render(data);
-  fs.writeFileSync(path.resolve(__dirname, '..', 'docs', fileName.replace('.md', '.html')), result);
+  const resultHTML = html.replace('{{CONTENT}}', result);
+  fs.writeFileSync(path.resolve(__dirname, '..', 'docs', fileName.replace('.md', '.html')), resultHTML);
 }
